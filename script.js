52a1f5144521595865844e5b80a313a15259fc0e
JPpixelado <dicasjogos2014@gmail.com>
2026-04-18 18:52:34 -0300

    Add download button alert in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo simples: alerta ao clicar no botão de download
+document.querySelector('#download .cta').addEventListener('click', () => {
+  alert('Obrigado por baixar Sexta à Noite do Funk!');
+});
